1fa821e1c75eb12130958231830ed9d37f23698b
Scott <scottmillan1@gmail.com>
2019-12-10 13:58:12 +0000

    removed whitespace

--- src/main/java/io/selectivesoftware/chipadvisor/ChipAdvisorApplication.java
+++ src/main/java/io/selectivesoftware/chipadvisor/ChipAdvisorApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChipAdvisorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChipAdvisorApplication.class, args);
-	}
-
+  public static void main(final String[] args) {
+    SpringApplication.run(ChipAdvisorApplication.class, args);
+  }
 }
