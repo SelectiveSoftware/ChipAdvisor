4a90cb336cecf34c1036688c619b158fb1ddb1de
Scott <scottmillan1@gmail.com>
2019-12-10 13:52:57 +0000

    intial bootstrap of the project

---
+++ src/main/java/io/selectivesoftware/chipadvisor/ChipAdvisorApplication.java
@@ -0,0 +1,13 @@
+package io.selectivesoftware.chipadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChipAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChipAdvisorApplication.class, args);
+	}
+
+}
